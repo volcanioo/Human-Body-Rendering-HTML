4808e32f457f48da5aa3c243c23bc1310ca71957
M. S. Deveci <arayuzgelisitirici@gmail.com>
2017-10-26 14:04:54 +0300

    edit command lines and upload gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+const base = 'Human-Body-SVG';
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: base
+        }
+    });
+});
+gulp.task('watch', function () {
+    gulp.watch(base + "/*.html").on("change", browserSync.reload);
+    gulp.watch(base + "/*.js").on("change", browserSync.reload);
+});
+gulp.task('default', ['watch', 'browser-sync']);
